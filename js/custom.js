5fe362a98b002f3efd0cfaec7bf81386ff5920e5
Whit Jack <whitj00@ymail.com>
2015-08-06 00:38:34 -0700

    Decrease load

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,3 @@
-/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
- * Author:HtmlCoder
- * Author URI:http://www.htmlcoder.me
- * Version:1.0.0
- * Created:November 2014
- * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
- * File Description: Place here your custom scripts
- */
 $.getJSON("https://www2.shapeshift.io/crowdsales", function (json) {
     var soldT = Math.round(json.sold);
     var left = json.remaining;
