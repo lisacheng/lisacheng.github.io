b36bc15b84a1f00018fc3f305c47a7ec469d327a
Nicola Minichiello <github@tutanota.de>
2015-08-12 06:07:31 +0100

    Added second progress bar
    Added second progress bar

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
+var raised;
+
 $(function(){
      $.getJSON("https://www2.shapeshift.io/crowdsales", function (json) {
          console.log(json);
+        var btcprice;
         var soldT = Math.round(json.sold);
         var left = json.remaining;
         var total = Math.round(soldT+left);
@@ -14,13 +17,24 @@ $(function(){
         var percentsold = Math.round(percent) + '%';
         var backers = json.orders + ' backers';
         var funded = Math.round(raised/730*100);
+        var percentRemaining = Math.round((left/total)*100);
+        var bcySold = 100-percentRemaining;
+        $(function(){
+        $.get( "https://api.bitcoinaverage.com/ticker/global/USD/last", function(data) {
+            var usdTotal = Math.round(raised*data)
+            $('.btcprice').html("$" + usdTotal + " raised at current rate");
+            });
+        });
         $('.progress-bar').css('width', percentsold);
-
         $('.ratebtc').html(ratebtc );  
         $('.backers').html(raisedtotal + " from " + backers );
-        $('.progress-bar-success').css('width', goal + '%');
+        $('.progress-bar-goal').css('width', goal + '%');
+        $('.progress-bar-bcy').css('width', bcySold + '%');
+        $('.bcybar').html(bcySold + "% BCY Sold");
         $('.goal-label').html(raisedtotal + " towards goal of 730 BTC (" + soldT + " out of " + total + " BCY)");
         $('.funded-label').html(funded + " % Funded");
+        $('.remainingpercent').html(percentRemaining + "% BCY Remaining");
+
     }); 
     
 });
@@ -52,5 +66,3 @@ var nextYear = moment.tz("2015-09-03 01:00", "ETC/UTC");
 $('#clock').countdown(nextYear.toDate(), function(event) {
   $(this).html(event.strftime('The token sale ends in\n %D Days, %H Hours, %M Minutes, and %S Seconds'));
 });
-
-
